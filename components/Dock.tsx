43147050a398ddc8e31461e6d2e081f611cf28e6
BalaTBR <101926371+invincibleinventor@users.noreply.github.com>
2024-12-12 01:54:16 +0530

    Update Dock.tsx

--- components/Dock.tsx
+++ components/Dock.tsx
@@ -58,14 +58,14 @@ const Dock = () => {
       id="launchpad"
       style={{ zIndex: 1000 }}
       className={`py-20 absolute w-screen h-screen bg-opacity-50 dark:bg-black dark:bg-opacity-50 bg-white backdrop-blur-lg`}
-      initial={{ opacity: 0 }}  // Initially hidden
-      animate={{ opacity: launchpad ? 1 : 0 }}  // Fade in/out based on `launchpad`
-      exit={{ opacity: 0 }}  // Fade out when leaving
-      transition={{ opacity: { duration: 0.1 } }}  // Control timing of fade and display change
+      initial={{ opacity: 0 }} 
+      animate={{ opacity: launchpad ? 1 : 0 }} 
+      exit={{ opacity: 0 }}  
+      transition={{ opacity: { duration: 0.1 } }}  
     >
         <div className='mx-auto relative w-56'>
         <svg xmlns="http://www.w3.org/2000/svg" className='absolute w-4 text-neutral-800 dark:text-neutral-400 left-4 h-4 top-0 bottom-0 my-auto' width="32" height="32" viewBox="0 0 32 32"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m5 27l7.5-7.5M28 13a9 9 0 1 1-18 0a9 9 0 0 1 18 0"/></svg>
-        <input className='px-2 pl-10 dark:text-white text-black placeholder:dark:text-neutral-400 outline-none focus:border-2 focus:border-blue-600 focus:dark:border-blue-300 transition-all ease-linear duration-75 placeholder:text-neutral-800 w-full text-xs rounded-md py-2 border border-neutral-200 dark:border dark:border-neutral-700 bg-white bg-opacity-20 dark:bg-black dark:bg-opacity-20' placeholder='Search Apps'></input>
+        <input className='px-2 pl-10 dark:text-white text-black placeholder:dark:text-neutral-400 outline-none focus:border-2 focus:border-blue-600 focus:dark:border-blue-300 transition-all ease-linear duration-75 placeholder:text-neutral-800 w-full text-xs rounded-lg py-2 bg-white bg-opacity-20 dark:bg-black dark:bg-opacity-20' placeholder='Search Apps'></input>
         </div>
       </motion.div>}
     <motion.div
